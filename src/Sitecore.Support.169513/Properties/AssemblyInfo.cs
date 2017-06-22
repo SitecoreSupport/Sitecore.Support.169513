0c8e0f9410a80190280ed809979f9979cd65b7ab
Dmitry Kolinchuk <dmko@sitecore.net>
2017-06-22 16:41:39 +0300

    Prevents System.NullReferenceException when custom controller action returns view with defined placeholder

---
+++ src/Sitecore.Support.169513/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.169513")]
+[assembly: AssemblyProduct("Sitecore.Support.169513")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.169513/RenderAddedContent.cs
@@ -0,0 +1,13 @@
+﻿namespace Sitecore.Support.Mvc.Pipelines.Response.RenderPlaceholder
+{
+    public class RenderAddedContent : Sitecore.Mvc.Pipelines.Response.RenderPlaceholder.RenderAddedContent
+    {
+        public override void Process(Sitecore.Mvc.Pipelines.Response.RenderPlaceholder.RenderPlaceholderArgs args)
+        {
+            if (args != null && args.OwnerRendering != null && args.OwnerRendering.Properties != null)
+            {
+                base.Process(args);
+            }
+        }
+    }
+}
